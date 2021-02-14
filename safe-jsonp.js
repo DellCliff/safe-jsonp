e87d9a54874573b7ad49f61382493b8d7fe6c664
DellCliff <dellcliff@gmx.ch>
2021-02-14 13:48:50 +0100

    Update safe-jsonp.js

--- safe-jsonp.js
+++ safe-jsonp.js
@@ -1,33 +1,82 @@
+const errorCodeOnWindowError = "a17fccef-ddcf-4386-8f31-05cd6d9490f0";
+const errorCodeOnScriptError = "ee8c8441-e594-440e-832e-8f3d023fbe87";
+const errorCodeOnTimeout = "be868a85-9ff3-4c69-8ee6-abd7576e3398";
+
 function createSandbox() {
-  return new Promise(success => {
-    const iframe = document.createElement("iframe");
-    iframe.hidden = true;
-    iframe.setAttribute("sandbox", "allow-scripts");
+  const iframe = document.createElement("iframe");
+  iframe.hidden = true;
+  iframe.setAttribute("sandbox", "allow-scripts");
 
-    {
-      const script = document.createElement("script");
-      script.textContent = `
-        window.onmessage = ({data}) => {
-          const script = document.createElement("script");
-          script.src = data.url;
+  {
+    const script = document.createElement("script");
+    script.textContent = `
+      window.onmessage = ({data}) => {
+        const script = document.createElement("script");
+        script.src = data.url;
+        let timeoutToken;
 
-          window[data.callbackName] = response => {
-            window.parent.postMessage({response, requestId: data.requestId}, "*");
-
+        function cleanup() {
+          delete window[data.callbackName];
+          script.onerror = null;
+          script.onload = null;
+          window.onerror = null;
+          clearTimeout(timeoutToken);
+          try {
             document.body.removeChild(script);
-            delete window[data.callbackName];
-          };
+          } catch (ignore) {}
+        }
 
-          document.body.appendChild(script);
+        let sentReply = false;
+
+        window.onerror = () => {
+          if (!sentReply) {
+            cleanup();
+            sentReply = true;
+            window.parent.postMessage({error: "${errorCodeOnWindowError}"}, "*");
+          }
         };
-      `;
 
-      const body = document.createElement("body");
-      body.appendChild(script);
+        script.onerror = () => {
+          if (!sentReply) {
+            cleanup();
+            sentReply = true;
+            window.parent.postMessage({error: "${errorCodeOnScriptError}"}, "*");
+          }
+        };
 
-      iframe.srcdoc = body.outerHTML;
-    }
+        script.onload = () => {
+          if (!sentReply) {
+            timeoutToken = setTimeout(
+              () => {
+                if (!sentReply) {
+                  cleanup();
+                  sentReply = true;
+                  window.parent.postMessage({error: "${errorCodeOnTimeout}"}, "*");
+                }
+              },
+              data.timeout);
+          }
+        };
 
+        window[data.callbackName] = response => {
+          if (!sentReply) {
+            cleanup();
+            sentReply = true;
+            window.parent.postMessage({response}, "*");
+          }
+        };
+
+        document.body.appendChild(script);
+      };
+    `;
+
+    const body = document.createElement("body");
+    body.appendChild(script);
+
+    iframe.srcdoc = body.outerHTML;
+  }
+
+  return new Promise(success => {
     iframe.onload = () => {
       success(iframe);
       iframe.onload = null;
@@ -37,26 +86,28 @@ function createSandbox() {
   });
 }
 
-function sandboxJsonp(sandbox, url, callbackName) {
-  return new Promise(success => {
-    const request = { url, callbackName, requestId: Math.random() };
-
+function sandboxJsonp(sandbox, url, callbackName, timeout) {
+  return new Promise((resolve, reject) => {
     function onMessage({source, data}) {
-      if (source !== sandbox.contentWindow || data.requestId !== request.requestId) {
+      if (source !== sandbox.contentWindow) {
         return;
       }
 
-      success(data.response);
+      if (data.response) {
+        resolve(data.response);
+      } else {
+        reject(data.error);
+      }
 
       window.removeEventListener('message', onMessage);
     }
     window.addEventListener('message', onMessage);
 
-    sandbox.contentWindow.postMessage(request, "*");
+    sandbox.contentWindow.postMessage({url, callbackName}, "*");
   });
 }
 
 export function jsonpSandbox() {
   const sandbox = createSandbox();
-  return async (url, callbackName) => await sandboxJsonp(await sandbox, url, callbackName);
+  return async (url, callbackName, timeout = 1000) => await sandboxJsonp(await sandbox, url, callbackName, timeout);
 }
